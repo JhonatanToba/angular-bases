19fbd508853e4b030ffad40f8c611c2e0abd6a9a
Jhonatan Leonardo Toba Silva <leonardotoba1998@gmail.com>
2024-03-27 20:10:06 -0500

    Add events and emit
    Se agregan los eventos y emisores de información desde varios componentes, así como se empiezan a verificar funcionamiento de los servicios

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -13,4 +13,14 @@ export class ListComponent {
     name: 'Trunks',
     power: 10
   }]
+
+  @Output()
+  public onDeleteId: EventEmitter<number> = new EventEmitter;
+
+  onDeleteCharacter(index:number):void{
+    // TODO: Emitir el ID del personaje
+
+    this.onDeleteId.emit(index);
+  }
+
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Character } from './../interfaces/character.interface';
+
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,14 +9,7 @@ import { Character } from './../interfaces/character.interface';
 
 export class MainPageComponent{
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name: 'Goku',
-    power: 9500
-  },{
-    name: 'Vegetta',
-    power: 7500
-  }]
+  constructor ( public dbzService: DbzService ) {
+
+  }
 }
